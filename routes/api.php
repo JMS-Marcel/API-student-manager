a509aa6d0d8cfb8477629596d62cebea26c7c792
jemshMarcel <jemshjms@gmail.com>
2024-10-30 20:39:50 +0300

    add api route for student authentificaton

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthAdminController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CoursController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\AuthAdminController;
+use App\Http\Controllers\StudentAuthController;
 
 Route::post('login', [AuthAdminController::class, 'login']);
 Route::post('logout', [AuthAdminController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('login-student', [StudentAuthController::class, 'login']);
+Route::post('logout-student', [StudentAuthController::class, 'logout'])->middleware('auth:sanctum');
 
 
 //Admin
