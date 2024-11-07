d0e39d9f96aa551407e27535a53a0c92dac6e767
jemshMarcel <jemshjms@gmail.com>
2024-11-07 23:28:57 +0300

    update link to auth

--- resources/js/components/navBar.vue
+++ resources/js/components/navBar.vue
@@ -111,18 +111,12 @@
                 </span>
               </label>
               <div class="hidden sm:flex">
-                <a
-                  href="signin.html"
-                  class="loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70"
+                <router-link
+                  to="/auth"
+                  class="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 ml-7 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark"
                 >
-                  Sign In
-                </a>
-                <a
-                  href="signup.html"
-                  class="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark"
-                >
-                  Sign Up
-                </a>
+                  Get Started
+                </router-link>
               </div>
             </div>
           </div>
