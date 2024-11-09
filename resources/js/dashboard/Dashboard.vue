e2bec78e625eb430552d8595df7aebbacdcf0b3b
jemshMarcel <jemshjms@gmail.com>
2024-11-09 18:33:35 +0300

    add variable to check if user is checked

--- resources/js/dashboard/Dashboard.vue
+++ resources/js/dashboard/Dashboard.vue
@@ -1,10 +1,11 @@
 <script setup>
+const role = localStorage.getItem("role")
 
 </script>
 <template>
   <!-- Need authentification -->
   <div class="w-full h-screen bg-slate-900">
-    <h1 class="text-5xl text-white text-center">DASHBOARD</h1>
-    <p class="text-center text-white p-4">Welcome!{{ role }}</p>
+    <h1 class="text-5xl text-white text-center border border-b-gray-400">DASHBOARD</h1>
+    <p class="text-center text-white p-4">Welcome! <span class="text-xl text-pink-500 shadow-lg-violet-500 ">{{ role }}</span></p>
   </div>
 </template>
