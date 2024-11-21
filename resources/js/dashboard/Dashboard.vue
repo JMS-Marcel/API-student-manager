5e7df4bd781a67b491553c6db37a6fc420e3fb4c
jemshMarcel <jemshjms@gmail.com>
2024-11-21 10:50:31 +0300

    refactor code in dashboard.vue

--- resources/js/dashboard/Dashboard.vue
+++ resources/js/dashboard/Dashboard.vue
@@ -2,8 +2,9 @@
 import axios from 'axios';
 import { useRouter } from 'vue-router';
 import { useSidebarStore } from '../../stores/sidebar';
+import HeaderArea from '../components/Header/HeaderArea.vue';
 
-const {toggleSidebar} = useSidebarStore()
+const { toggleSidebar } = useSidebarStore()
 const sidebarStore = useSidebarStore()
 
 const router = useRouter()
@@ -12,264 +13,34 @@ const token = localStorage.getItem("token")
 
 
 const logout = async () => {
-  await axios.delete(`api/logout-${role}`, {headers:{
-    Authorization:`Bearer ${token}`,
-  }})
-  .then((res)=> {          
-    if (res.data.status === 200) {
+  await axios.delete(`api/logout-${role}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
+    .then((res) => {
+      if (res.data.status === 200) {
         localStorage.removeItem('token')
         localStorage.removeItem('role')
         alert(res.data.message)
         router.push('/auth')
       }
-  })
-  .catch((error) => console.log(error))
+    })
+    .catch((error) => console.log(error))
 }
 
 </script>
 <template>
-    <header
-    class="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none"
-  >
-    <div class="flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11">
-      <div class="flex items-center gap-2 sm:gap-4 lg:hidden">
-        <!-- Hamburger Toggle BTN -->
-        <button
-          class="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
-          @click="
-            () => {
-              console.log('Toggling Sidebar')
-              toggleSidebar()
-            }
-          "
-        >
-          <span class="relative block h-5.5 w-5.5 cursor-pointer">
-            <span class="block absolute right-0 h-full w-full">
-              <span
-                class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white"
-                :class="{ '!w-full delay-300': !sidebarStore.isSidebarOpen }"
-              ></span>
-              <span
-                class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white"
-                :class="{ '!w-full delay-400': !sidebarStore.isSidebarOpen }"
-              ></span>
-              <span
-                class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white"
-                :class="{ '!w-full delay-500': !sidebarStore.isSidebarOpen }"
-              ></span>
-            </span>
-            <span class="block absolute right-0 h-full w-full rotate-45">
-              <span
-                class="absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white"
-                :class="{ '!h-0 delay-[0]': !sidebarStore.isSidebarOpen }"
-              ></span>
-              <span
-                class="delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white"
-                :class="{ '!h-0 delay-200': !sidebarStore.isSidebarOpen }"
-              ></span>
-            </span>
-          </span>
-        </button>
-        <!-- Hamburger Toggle BTN -->
-        <router-link class="block flex-shrink-0 lg:hidden" to="/">
-          <img src="../../assets/images/logo/logo-icon.svg" alt="Logo" />
-        </router-link>
-      </div>
-      <div class="hidden sm:block">
-        <form action="https://formbold.com/s/unique_form_id" method="POST">
-          <div class="relative">
-            <button class="absolute top-1/2 left-0 -translate-y-1/2">
-              <svg
-                class="fill-body hover:fill-primary dark:fill-bodydark dark:hover:fill-primary"
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M9.16666 3.33332C5.945 3.33332 3.33332 5.945 3.33332 9.16666C3.33332 12.3883 5.945 15 9.16666 15C12.3883 15 15 12.3883 15 9.16666C15 5.945 12.3883 3.33332 9.16666 3.33332ZM1.66666 9.16666C1.66666 5.02452 5.02452 1.66666 9.16666 1.66666C13.3088 1.66666 16.6667 5.02452 16.6667 9.16666C16.6667 13.3088 13.3088 16.6667 9.16666 16.6667C5.02452 16.6667 1.66666 13.3088 1.66666 9.16666Z"
-                  fill=""
-                />
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M13.2857 13.2857C13.6112 12.9603 14.1388 12.9603 14.4642 13.2857L18.0892 16.9107C18.4147 17.2362 18.4147 17.7638 18.0892 18.0892C17.7638 18.4147 17.2362 18.4147 16.9107 18.0892L13.2857 14.4642C12.9603 14.1388 12.9603 13.6112 13.2857 13.2857Z"
-                  fill=""
-                />
-              </svg>
-            </button>
+  <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+    <HeaderArea />
+    <!-- Sidebar -->
 
-            <input
-              type="text"
-              placeholder="Type to search..."
-              class="w-full xl:w-125 bg-transparent pr-4 pl-9 focus:outline-none"
-            />
-          </div>
-        </form>
-      </div>
+    <!-- Sidebar end -->
 
-      <div class="flex items-center gap-3 2xsm:gap-7">
-        <ul class="flex items-center gap-2 2xsm:gap-4">
-          <li>
-            <!-- Dark Mode Toggler -->
-            <!-- <DarkModeSwitcher /> -->
-            <!-- Dark Mode Toggler -->
-          </li>
-
-          <!-- Notification Menu Area -->
-          <!-- <DropdownNotification /> -->
-          <!-- Notification Menu Area -->
-
-          <!-- Chat Notification Area -->
-          <!-- <DropdownMessage /> -->
-          <!-- Chat Notification Area -->
-        </ul>
-
-        <!-- User Area -->
-        <DropdownUser />
-        <!-- User Area -->
-      </div>
-    </div>
-  </header>
-  <!-- Header -->
-       <div
-      class="ud-header absolute left-0 top-0 z-40 flex w-full items-center bg-transparent"
-    >
-      <div class="container">
-        <div class="relative -mx-4 flex items-center justify-between">
-          <div class="w-60 max-w-full px-4">
-            <a href="#" class="navbar-logo block w-full py-5">
-              <img
-                src="/logo.svg"
-                alt="logo"
-                class="header-logo w-full"
-              />
-            </a>
-          </div>
-          <div class="flex w-full items-center justify-between px-4">
-            <div>
-              <button
-                id="navbarToggler"
-                class="absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
-              >
-                <span
-                  class="relative my-[6px] block h-[2px] w-[30px] bg-white"
-                ></span>
-                <span
-                  class="relative my-[6px] block h-[2px] w-[30px] bg-white"
-                ></span>
-                <span
-                  class="relative my-[6px] block h-[2px] w-[30px] bg-white"
-                ></span>
-              </button>
-              <nav
-                id="navbarCollapse"
-                class="absolute right-4 top-full hidden w-full max-w-[250px] rounded-lg bg-white py-5 shadow-lg dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:px-4 lg:py-0 lg:shadow-none dark:lg:bg-transparent xl:px-6"
-              >
-
-
-              </nav>
-            </div>
-            <div class="flex items-center justify-end pr-16 lg:pr-0">
-              <label
-                for="themeSwitcher"
-                class="inline-flex cursor-pointer items-center"
-                aria-label="themeSwitcher"
-                name="themeSwitcher"
-              > 
-                <input
-                  type="checkbox"
-                  name="themeSwitcher"
-                  id="themeSwitcher"
-                  class="sr-only"
-                />
-                <span class="block text-white dark:hidden">
-                  <svg
-                    class="fill-current"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M13.3125 1.50001C12.675 1.31251 12.0375 1.16251 11.3625 1.05001C10.875 0.975006 10.35 1.23751 10.1625 1.68751C9.93751 2.13751 10.05 2.70001 10.425 3.00001C13.0875 5.47501 14.0625 9.11251 12.975 12.525C11.775 16.3125 8.25001 18.975 4.16251 19.0875C3.63751 19.0875 3.22501 19.425 3.07501 19.9125C2.92501 20.4 3.15001 20.925 3.56251 21.1875C4.50001 21.75 5.43751 22.2 6.37501 22.5C7.46251 22.8375 8.58751 22.9875 9.71251 22.9875C11.625 22.9875 13.5 22.5 15.1875 21.5625C17.85 20.1 19.725 17.7375 20.55 14.8875C22.1625 9.26251 18.975 3.37501 13.3125 1.50001ZM18.9375 14.4C18.2625 16.8375 16.6125 18.825 14.4 20.0625C12.075 21.3375 9.41251 21.6 6.90001 20.85C6.63751 20.775 6.33751 20.6625 6.07501 20.55C10.05 19.7625 13.35 16.9125 14.5875 13.0125C15.675 9.56251 15 5.92501 12.7875 3.07501C17.5875 4.68751 20.2875 9.67501 18.9375 14.4Z"
-                    />
-                  </svg>
-                </span>
-                <span class="hidden text-white dark:block">
-                  <svg
-                    class="fill-current"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g clip-path="url(#clip0_2172_3070)">
-                      <path
-                        d="M12 6.89999C9.18752 6.89999 6.90002 9.18749 6.90002 12C6.90002 14.8125 9.18752 17.1 12 17.1C14.8125 17.1 17.1 14.8125 17.1 12C17.1 9.18749 14.8125 6.89999 12 6.89999ZM12 15.4125C10.125 15.4125 8.58752 13.875 8.58752 12C8.58752 10.125 10.125 8.58749 12 8.58749C13.875 8.58749 15.4125 10.125 15.4125 12C15.4125 13.875 13.875 15.4125 12 15.4125Z"
-                      />
-                      <path
-                        d="M12 4.2375C12.45 4.2375 12.8625 3.8625 12.8625 3.375V1.5C12.8625 1.05 12.4875 0.637497 12 0.637497C11.55 0.637497 11.1375 1.0125 11.1375 1.5V3.4125C11.175 3.8625 11.55 4.2375 12 4.2375Z"
-                      />
-                      <path
-                        d="M12 19.7625C11.55 19.7625 11.1375 20.1375 11.1375 20.625V22.5C11.1375 22.95 11.5125 23.3625 12 23.3625C12.45 23.3625 12.8625 22.9875 12.8625 22.5V20.5875C12.8625 20.1375 12.45 19.7625 12 19.7625Z"
-                      />
-                      <path
-                        d="M18.1125 6.74999C18.3375 6.74999 18.5625 6.67499 18.7125 6.48749L19.9125 5.28749C20.25 4.94999 20.25 4.42499 19.9125 4.08749C19.575 3.74999 19.05 3.74999 18.7125 4.08749L17.5125 5.28749C17.175 5.62499 17.175 6.14999 17.5125 6.48749C17.6625 6.67499 17.8875 6.74999 18.1125 6.74999Z"
-                      />
-                      <path
-                        d="M5.32501 17.5125L4.12501 18.675C3.78751 19.0125 3.78751 19.5375 4.12501 19.875C4.27501 20.025 4.50001 20.1375 4.72501 20.1375C4.95001 20.1375 5.17501 20.0625 5.32501 19.875L6.52501 18.675C6.86251 18.3375 6.86251 17.8125 6.52501 17.475C6.18751 17.175 5.62501 17.175 5.32501 17.5125Z"
-                      />
-                      <path
-                        d="M22.5 11.175H20.5875C20.1375 11.175 19.725 11.55 19.725 12.0375C19.725 12.4875 20.1 12.9 20.5875 12.9H22.5C22.95 12.9 23.3625 12.525 23.3625 12.0375C23.3625 11.55 22.95 11.175 22.5 11.175Z"
-                      />
-                      <path
-                        d="M4.23751 12C4.23751 11.55 3.86251 11.1375 3.37501 11.1375H1.50001C1.05001 11.1375 0.637512 11.5125 0.637512 12C0.637512 12.45 1.01251 12.8625 1.50001 12.8625H3.41251C3.86251 12.8625 4.23751 12.45 4.23751 12Z"
-                      />
-                      <path
-                        d="M18.675 17.5125C18.3375 17.175 17.8125 17.175 17.475 17.5125C17.1375 17.85 17.1375 18.375 17.475 18.7125L18.675 19.9125C18.825 20.0625 19.05 20.175 19.275 20.175C19.5 20.175 19.725 20.1 19.875 19.9125C20.2125 19.575 20.2125 19.05 19.875 18.7125L18.675 17.5125Z"
-                      />
-                      <path
-                        d="M5.32501 4.125C4.98751 3.7875 4.46251 3.7875 4.12501 4.125C3.78751 4.4625 3.78751 4.9875 4.12501 5.325L5.32501 6.525C5.47501 6.675 5.70001 6.7875 5.92501 6.7875C6.15001 6.7875 6.37501 6.7125 6.52501 6.525C6.86251 6.1875 6.86251 5.6625 6.52501 5.325L5.32501 4.125Z"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_2172_3070">
-                        <rect width="24" height="24" fill="white" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </span>
-              </label>
-              <div class="hidden sm:flex">
-                <form method="post" @submit.prevent="logout">
-                  <button type="submit" class="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 ml-7 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">Log out</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  <!-- Header end -->
-
-  <!-- Sidebar -->
-
-  <!-- Sidebar end -->
-   
-  <!-- Main -->
-   <main>
-    <slot></slot>
-     <div class="w-full h-screen bg-slate-900">
-       <h1 class="text-5xl text-white text-center">DASHBOARD</h1>
-       <p class="text-center text-white p-4">Welcome! <span class="text-xl text-pink-500 shadow-lg-violet-500 ">{{ role }}</span></p>
-     </div>
-   </main>
-  <!-- Main end-->
-
+    <!-- Main -->
+    <main>
+      <slot></slot>
+    </main>
+    <!-- Main end-->
+  </div>
 </template>
