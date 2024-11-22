77700c1154399dc001b3a50cc4ecbb11efe0b77e
jemshMarcel <jemshjms@gmail.com>
2024-11-22 12:24:06 +0300

    add selected on sidebar

--- resources/stores/sidebar.ts
+++ resources/stores/sidebar.ts
@@ -4,6 +4,7 @@ import { ref } from "vue";
 
 export const useSidebarStore = defineStore('sidebar', () =>{
   const isSidebarOpen = ref(false)
+  const selected = useStorage('selected', ref('eCommerce'))
   const page = useStorage('page', ref('Dashboard'))
 
   function toggleSidebar(){
