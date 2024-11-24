243a80d93163659f028b6a74b7c74891eca0f2a0
jemshMarcel <jemshjms@gmail.com>
2024-11-24 07:28:52 +0300

    add ChartOne in Dashboard

--- resources/js/dashboard/Dashboad.vue
+++ resources/js/dashboard/Dashboad.vue
@@ -1,12 +1,16 @@
 <script setup>
 import DefaultLayout from '../Layout/DefaultLayout.vue';
 import DataStatsOne from '../components/DataStats/DataStatsOne.vue';
+import ChartOne from '../components/Charts/ChartOne.vue';
 </script>
 
 <template>
   <DefaultLayout>
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
       <DataStatsOne />
+    </div>
+    <div class="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+      <ChartOne/>
     </div>
   </DefaultLayout>
 </template>
