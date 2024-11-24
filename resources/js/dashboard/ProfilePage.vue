ac9c43beb2b9a42428bd1060a235527b51b1864d
jemshMarcel <jemshjms@gmail.com>
2024-11-24 17:29:10 +0300

    import  BreadcrumbDefault component

--- resources/js/dashboard/ProfilePage.vue
+++ resources/js/dashboard/ProfilePage.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 import DefaultLayout from '../Layout/DefaultLayout.vue'
 import ProfileCard from '../components/ProfileCard.vue'
+import BreadcrumbDefault from '../components/Breadcrumbs/BreadcrumbDefault.vue'
+
 
 const pageTitle = ref('Profile')
 </script>
