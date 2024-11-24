0e878617988519dcfff94aca97e8eae46e81585b
jemshMarcel <jemshjms@gmail.com>
2024-11-24 14:44:09 +0300

    Created new components: -ProfileCard.vue -SettingCard.vue -BreadcrumbDefault.vue -ProfilePage -SettingPage

---
+++ resources/js/dashboard/ProfilePage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import DefaultLayout from '../Layout/DefaultLayout.vue'
+import ProfileCard from '../components/ProfileCard.vue'
+
+const pageTitle = ref('Profile')
+</script>
+
+<template>
+  <DefaultLayout>
+    <div class="mx-auto max-w-242.5">
+      <BreadcrumbDefault :pageTitle="pageTitle"/>
+
+      <ProfileCard/>
+    </div>
+  </DefaultLayout>
+
+</template>
---
+++ resources/js/dashboard/SettingPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import DefaultLayout from '../Layout/DefaultLayout.vue'
+import SettingCard from '../components/SettingCard.vue'
+
+const pageTitle = ref('Settings Page')
+</script>
+
+<template>
+  <DefaultLayout>
+    <div class="mx-auto max-w-242.5">
+      <BreadcrumbDefault :pageTitle="pageTitle"/>
+
+      <SettingCard/>
+    </div>
+  </DefaultLayout>
+
+</template>
