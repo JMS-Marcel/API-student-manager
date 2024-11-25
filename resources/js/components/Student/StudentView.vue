e46e0ac880c01d376ea9b60ffa27380e0fa1996d
jemshMarcel <jemshjms@gmail.com>
2024-11-25 17:19:22 +0300

    create StudentView.vue component

---
+++ resources/js/components/Student/StudentView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import StudentDefault from '../../dashboard/StudentDefault.vue';
+
+</script>
+<template>
+  <StudentDefault>
+    <template #student>
+        <h1>Student View</h1>
+    </template>
+  </StudentDefault>
+</template>
