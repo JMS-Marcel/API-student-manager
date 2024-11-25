40529af396f07da1483d5e97d7b7ac58ff83d6d5
jemshMarcel <jemshjms@gmail.com>
2024-11-25 17:18:03 +0300

    create StudentEdit.vue component

---
+++ resources/js/components/Student/StudentEdit.vue
@@ -0,0 +1,11 @@
+<script setup>
+import StudentDefault from '../../dashboard/StudentDefault.vue';
+
+</script>
+<template>
+  <StudentDefault>
+    <template #student>
+        <h1>Student Edit</h1>
+    </template>
+  </StudentDefault>
+</template>
