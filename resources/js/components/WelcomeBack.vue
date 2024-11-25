5353e63a5b1062e359944b3496f8c154e54d03a2
jemshMarcel <jemshjms@gmail.com>
2024-11-25 17:06:39 +0300

    Add new component welcomeBack

---
+++ resources/js/components/WelcomeBack.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from 'vue'
+import { useUserCurrentlyStore } from '../../stores/UserCurrently'
+
+const UserCurrently = useUserCurrentlyStore()
+
+
+computed(() => UserCurrently.getUserCurrently())
+
+</script>
+<template>
+  <div
+    class="rounded-md border border-stroke bg-white py-4 px-7.5 mb-6 shadow-default dark:border-strokedark dark:bg-boxdark">
+    <h1 class="text-title-md font-bold text-black dark:text-white">Welcome Back, {{ UserCurrently.data.nom+ ' ' +UserCurrently.data.prenom }}</h1>
+    <p class="text-sm font-medium">Have a Goood day at work</p>
+  </div>
+</template>
--- resources/js/dashboard/Dashboad.vue
+++ resources/js/dashboard/Dashboad.vue
@@ -3,10 +3,12 @@ import DefaultLayout from '../Layout/DefaultLayout.vue';
 import DataStatsOne from '../components/DataStats/DataStatsOne.vue';
 import ChartOne from '../components/Charts/ChartOne.vue';
 import ChartTwo from '../components/Charts/ChartTwo.vue';
+import WelcomeBack from '../components/WelcomeBack.vue';
 </script>
 
 <template>
   <DefaultLayout>
+    <WelcomeBack />
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
       <DataStatsOne />
     </div>
