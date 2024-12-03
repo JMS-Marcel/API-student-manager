b2c07aef25b31398b936fcccc9e3ea9b0ba35365
jemshMarcel <jemshjms@gmail.com>
2024-12-03 19:41:15 +0300

    Add new template for coursAdd

--- resources/js/components/Cours/CoursAdd.vue
+++ resources/js/components/Cours/CoursAdd.vue
@@ -1,13 +1,55 @@
 <script setup>
-import { ref } from 'vue';
-import PageActionDefault from '../../Layout/PageActionDefault.vue';
+import { ref } from 'vue'
+import DefaultLayout from '../../Layout/DefaultLayout.vue';
+import DefaultCard from '../DashboardForms/DefaultCard.vue';
+import BreadcrumbDefault from '../Breadcrumbs/BreadcrumbDefault.vue';
+import InputGroup from '../DashboardForms/InputGroup.vue';
 
-const pageTitle = ref('Cours Add')
+
+
+const pageTitle = ref('Form Layout')
 </script>
 <template>
-  <PageActionDefault :pageTitle="pageTitle">
+  <DefaultLayout>
+    <BreadcrumbDefault :pageTitle="pageTitle" />
+        <!-- ====== Form Layout Section Start -->
+        <div class="grid">
+      <div class="flex flex-col gap-9">
+        <!-- Contact Form Start -->
+        <DefaultCard cardTitle="Contact Form">
+          <form action="#">
+            <div class="p-6.5">
+              <div class="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                <InputGroup
+                  label="Nom"
+                  type="text"
+                  placeholder="Enter your first name"
+                  customClasses="w-full xl:w-1/2"
+                  required
+                />
 
-        <h1>Cours ADD</h1>
+                <InputGroup
+                  label="Description"
+                  type="text"
+                  placeholder="Enter your last name"
+                  customClasses="w-full xl:w-1/2"
+                  required
+                />
+              </div>
 
-  </PageActionDefault>
+              <button
+                class="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90"
+              >
+                Add
+              </button>
+            </div>
+          </form>
+        </DefaultCard>
+        <!-- Contact Form End -->
+      </div>
+   
+    </div>
+    <!-- ====== Form Layout Section End -->
+  </DefaultLayout>
+
 </template>
