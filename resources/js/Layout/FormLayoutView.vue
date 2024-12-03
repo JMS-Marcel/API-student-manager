04d8b9e96bfc23dde89a57e1d80b74d25225fd90
jemshMarcel <jemshjms@gmail.com>
2024-12-03 19:45:34 +0300

    Create new component "FormLayoutView.vue"

---
+++ resources/js/Layout/FormLayoutView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import DefaultLayout from './DefaultLayout.vue'
+import DefaultCard from '../components/DashboardForms/DefaultCard.vue';
+import BreadcrumbDefault from '../components/Breadcrumbs/BreadcrumbDefault.vue';
+import InputGroup from '../components/DashboardForms/InputGroup.vue';
+import { ref } from 'vue';
+
+const pageTitle = ref('Form Layout')
+</script>
